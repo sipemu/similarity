e37c3f5a9bb67a45f52fd66bb95266073d3970bf
Simon Müller <info@muon-stat.com>
2016-09-19 23:22:24 +0200

    add terminal node id test

---
+++ tests/testthat/test-RFDistance.R
@@ -0,0 +1,9 @@
+testthat::context("Ranger Proximity")
+
+testthat::test_that("Terminal Node IDs", {
+  set.seed((1234))
+  rf <- ranger::ranger(Species ~ ., data = iris, num.trees = 5, write.forest = TRUE)
+  tn1 <- terminalNodeIdsRanger(iris[, -5], rf)
+  tn2 <- ranger::getTerminalNodeIDs(rf, iris[, -5])
+  testthat::expect_equal(tn1, tn2)
+})
