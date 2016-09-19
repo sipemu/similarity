428f543fd567e6e862789013cc0dbcd721b4c9fe
Simon Müller <info@muon-stat.com>
2016-09-19 22:47:58 +0200

    update testthat

--- tests/testthat.R
+++ tests/testthat.R
@@ -1,11 +1,2 @@
 library(testthat)
 library(Similarity)
-
-test_that("Goodall 1 Distance", {
-  set.seed(123)
-  x <- matrix(rnorm(100), 10, 10)
-  # + 1 as index starts from 0
-  y1 <- test_rank_matrix(x) + 1
-  y2 <- apply(x, 2, order)
-  expect_equal(y1, y2)
-})
